beb862b22c66aa304c5ad2e33b4d38c8d6450b07
Miroslav <86550081+mmIIrrOOss@users.noreply.github.com>
2022-08-10 19:55:12 +0300

    Refactoring to your code=Just apled best practice.

--- Director.cs
+++ Director.cs
@@ -1,106 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Data;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Dapper;
 
 namespace SchoolProject
 {
     public class Director
     {
+        private const string ValidationRegexNamePattern = "^[A-Z][a-zA-Z]*$";
+
         private int directorId;
         private string firstName;
         private string lastName;
 
-        public int DirectorId { get; set; }
-        public string FirstName
-        {
-            get { return firstName; }
-            set
-            {
-                firstName = value;
-                if (!Regex.Match(firstName, "^[A-Z][a-zA-Z]*$").Success)
-                    Console.WriteLine("Invalid Firstname.");
-            }
-
-
-        }
-        public string LastName
-        {
-            get { return lastName; }
-            set
-            {
-                lastName = value;
-                if (!Regex.Match(lastName, "^[A-Z][a-zA-Z]*$").Success)
-                    Console.WriteLine("Invalid Lastname.");
-            }
-        }
 
         public Director(int directorId, string firstName, string lastName)
         {
-            this.directorId = directorId;
-            this.firstName = firstName;
-            this.lastName = lastName;
-
-           InsertDirector();
+            this.DirectorId = directorId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
         }
 
-        private void InsertDirector()
+
+        public int DirectorId { get; set; }
+
+        public string FirstName
         {
-            try
+            get
             {
-                var insertPersonQuery = "dbo.InsertDirector @directorId, @firstName, @lastName";
-                using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("StoreDb")))
-                {
-                    connection.Execute(insertPersonQuery, new
-                    {
-                        directorId = directorId,
-                        firstName = firstName,
-                        lastName = lastName,
-                        
-                    });
-
-                }
+                return this.firstName;
             }
-            catch (Exception ex)
+            set
             {
-                Console.WriteLine("Already Inserted");
+                if (ValidateName(value))
+                {
+                    Console.WriteLine("Invalid Firstname!");
+                    return;
+                }
+                this.firstName = value;
             }
         }
-            public void RemoveTeacher (int teacherId)
+
+      
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                if (ValidateName(value))
+                {
+                    Console.WriteLine("Invalid Lastname!");
+                    return;
+                }
+                this.lastName = value;
+            }
+        }
+
+       
+
+        public void RemoveTeacher(int teacherId)
         {
             RemoveTeacherPrivate(teacherId);
         }
 
-        public void RemoveStudent (int studentNum)
+        public void RemoveStudent(int studentNum)
         {
 
-            RemoveStudentPrivate(studentNum);            
-        }
-
-        private void RemoveTeacherPrivate(int teacherId)
-        {
-            var insertPersonQuery = "dbo.RemoveTeacher @teacherId";
-            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("StoreDB")))
-            {
-                connection.Execute(insertPersonQuery, new { teacherId = teacherId });
-
-            }
-        }
-
-
-        private void RemoveStudentPrivate(int studentNum)
-        {
-            var insertPersonQuery = "dbo.RemoveStudent @studentNum";
-            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("StoreDB")))
-            {
-                connection.Execute(insertPersonQuery, new { studentNum = studentNum });
-
-            }
-
+            RemoveStudentPrivate(studentNum);
         }
 
         public void AddWarningToTeacher()
@@ -113,19 +82,79 @@ namespace SchoolProject
 
         }
 
+        private static bool ValidateName(string value)
+        {
+            return !Regex.Match(value, ValidationRegexNamePattern).Success;
+        }
+
+        private void InsertDirector()
+        {
+            try
+            {
+                var insertPersonQuery = "dbo.InsertDirector @directorId, @firstName, @lastName";
+                using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("StoreDb")))
+                {
+                    connection.Execute(insertPersonQuery, new
+                    {
+                        DirectorId = DirectorId,
+                        FirstName = FirstName,
+                        LastName = LastName,
+
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Already Inserted!");
+            }
+        }
+
+        private void RemoveTeacherPrivate(int teacherId)
+        {
+            var insertPersonQuery = "dbo.RemoveTeacher @teacherId";
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("StoreDB")))
+            {
+                connection.Execute(insertPersonQuery, new
+                {
+                    teacherId = teacherId
+                });
+            }
+        }
+
+        private void RemoveStudentPrivate(int studentNum)
+        {
+            var insertPersonQuery = "dbo.RemoveStudent @studentNum";
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("StoreDB")))
+            {
+                connection.Execute(insertPersonQuery, new
+                {
+                    studentNum = studentNum
+                });
+            }
+        }
+
         private void InsertWarningToTeacher()
         {
-            
+
             Console.Write("Insert your Valid Id: ");
             int directorId = int.Parse(Console.ReadLine());
+
             Console.Write("Insert Teacher Valid Id:");
             int teacherId = int.Parse(Console.ReadLine());
+
             Console.Write("Insert text warning:");
             string warningText = Console.ReadLine();
+
             var insertPersonQuery = "dbo.InsertTeacherWarning @warningText, @directorId, @teacherId";
             using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("StoreDB")))
             {
-                connection.Execute(insertPersonQuery, new { warningText = warningText, directorId = directorId, teacherId = teacherId  });
+                connection.Execute(insertPersonQuery,
+                    new
+                    {
+                        warningText = warningText,
+                        directorId = directorId,
+                        teacherId = teacherId
+                    });
 
             }
         }
@@ -134,8 +163,6 @@ namespace SchoolProject
         {
 
         }
-
-
 
 
     }
